6373d957f42eff7f090e04960389ebe9fb16b0ed
Yao <48658585+0xYao@users.noreply.github.com>
2022-06-30 15:15:23 +1200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -145,7 +145,7 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>— you just made a Gatsby App! </span>
         🎉🎉🎉
       </h1>
       <p style={paragraphStyles}>
